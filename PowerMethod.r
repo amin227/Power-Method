ac7530919c16fc05443a5e338bdffd177b95455e
amin227 <38471224+amin227@users.noreply.github.com>
2018-12-16 22:57:29 +0700

    Add files via upload

---
+++ PowerMethod.r
@@ -0,0 +1,12 @@
+
+A<- matrix(c(4,-5,2,-3),2,2, byrow= TRUE)
+A
+
+library(matlib)
+powerMethod(A)
+
+B<- matrix(c(0,11,-5,-2,17,-7,-4,26,-10),3,3, byrow= TRUE)
+B
+
+library(matlib)
+powerMethod(B)
